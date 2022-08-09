09c5f79aef4d2363ddd650ff9eae83fb873ab100
= <=>
2022-08-09 15:33:26 -0500

    aded logic whastapp

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,7 +79,8 @@ popupWhatsApp = () => {
    let btnClosePopup = document.querySelector('.closePopup');
    let btnOpenPopup = document.querySelector('.whatsapp-button');
    let popup = document.querySelector('.popup-whatsapp');
-   let sendBtn = document.getElementById('send-btn');
+   let sendBtn_1 = document.getElementById('send-btn-1');
+   let sendBtn_2 = document.getElementById('send-btn-2');
 
    btnClosePopup.addEventListener("click", () => {
       popup.classList.toggle('is-active-whatsapp-popup')
@@ -90,13 +91,19 @@ popupWhatsApp = () => {
       popup.style.animation = "fadeIn .6s 0.0s both";
    })
 
-   sendBtn.addEventListener("click", () => {
+   sendBtn_1.addEventListener("click", () => {
       let msg = document.getElementById('whats-in').value;
       let relmsg = msg.replace(/ /g, "%20");
       //just change the numbers "1515551234567" for your number. Don't use +001-(555)1234567     
-      window.open('https://wa.me/573202844903?text=' + relmsg, '_blank');
-
+      window.open('https://wa.me/573006815390?text=' + relmsg, '_blank');
    });
+
+   sendBtn_2.addEventListener("click", () => {
+    let msg = document.getElementById('whats-in').value;
+    let relmsg = msg.replace(/ /g, "%20");
+    //just change the numbers "1515551234567" for your number. Don't use +001-(555)1234567     
+    window.open('https://wa.me/573115685780?text=' + relmsg, '_blank');
+ });
 
    /* Open pop-up in 15 seconds */
    /* setTimeout(() => {
